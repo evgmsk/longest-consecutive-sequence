c712a24e07050bd2decb695aa875104d508a351c
evg <em1777933@gmail.com>
2018-03-02 00:59:54 +0300

    longest-sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    const length = array.length;
+    if(length < 2)
+        return length;
+    const arr = [ ...array].sort((a,b) => a-b);
+    let [f, s] = [1, 0];
+    for(let i = 1; i < length; i++){
+        if(arr[i] - 1 === arr[i - 1]){
+            ++f
+        }else if (arr[i] - arr[i - 1] > 1){
+            s = s < f? f: s;
+            f = 1
+        }
+    }
+    return  s = s < f? f: s;
+};
