8de650b4d83e735666af6fe0cb6271ce664beef2
evg <em1777933@gmail.com>
2018-03-04 17:29:11 +0300

    last commit

--- src/index.js
+++ src/index.js
@@ -1,157 +1,64 @@
-module.exports = function longestConsecutiveLength(array) {
+module.exports = function longestConsecutiveLength(array, sort = false) {
     const length = array.length;
     if(length < 2)
         return length;
 
-    // Algorithm with sort more fast and universal solution
-    /*
-    const arr = [ ...array].sort((a,b) => a-b);
-    let [f, s] = [1, 0];
-    for(let i = 1; i < length; i++){
-        if(arr[i] - 1 === arr[i - 1]){
-            ++f
-        }else if (arr[i] - arr[i - 1] > 1){
-            s = s < f? f: s;
-            f = 1
-        }
-    }
-    return  s = s < f? f: s;*/
-     //If next algorithm such as you need,
-    // i want to notice, it more slow then previous with sort
-    let result = 1;
     const hash = {};
-    for(x of array){
-        let key1 = Math.floor((x/length));
-        let key2 = x%length;
-        if(!hash[key1])
-            hash[key1] = {};
-        hash[key1][key2] = true;
-        let [i,j] = [key1, key2];
-        let count = 1;
-        while(true){
-            if(j){
-                j--;
-                if(!hash[i][j])
-                    break;
-                else{
-                    count++
-                }
-            }else{
-                if(!i)
-                    break;
-                else{
-                    i--;
-                    j = length-1;
-                    if(!hash[i][j])
-                        break;
-                    else{
-                        count++
-                    }
-                }
-            }
-        }
-        [i,j] = [key1,key2];
-        while(true){
-            if(j < length){
-                j++;
-                if(!hash[i][j])
-                    break;
-                else{
-                    count++
-                }
-            }else{
-                if(i < length)
-                    break;
-                else{
-                    i++;
-                    j = 0;
-                    if(!hash[i][j])
-                        break;
-                    else{
-                        count++
-                    }
-                }
-            }
-        }
-        result = result> count? result: count
 
-    }
-    //This algorithm guarantee fast resolving
-    // if there are long consecutive sequence in array,
-    // but in your cases it more slow then previous
-    /*
-    const hash = {};
-    const length = array.length;
-    let result = 1;
-    if(length < 2)
-        return length;
-    for(x of array) {
-        let key1 = Math.floor((x / length));
-        let key2 = x % length;
-        if (!hash[key1])
-            hash[key1] = {};
-        hash[key1][key2] = true;
-    }
-    for( let x of Object.keys(hash)){
-        const keys = Object.keys(hash[x]);
-        //console.log(keys)
-        if(keys.length > result){
-            for( let y of keys) {
-                let count = 1;
-                hash[x][y] = false;
-                let [i,j] = [x, y];
-                while(true){
-                    if(j){
-                        j--;
-                        if(!hash[i][j])
-                            break;
-                        else {
-                            hash[i][j] = false;
-                            count++
-                        }
-                    }else{
-                        if(!i)
-                            break;
-                        else{
-                            i--;
-                            j = length-1;
-                            if(!hash[i][j])
-                                break;
-                            else{
-                                hash[i][j] = false;
-                                count++
-                            }
-                        }
-                    }
-                }
-                [i,j] = [x, y];
-                while(true){
-                    if(j < length){
-                        j++;
-                        if(!hash[i][j])
-                            break;
-                        else{
-                            hash[i][j] = false;
-                            count++
-                        }
-                    }else{
-                        if(i < length)
-                            break;
-                        else{
-                            i++;
-                            j = 0;
-                            if(!hash[i][j])
-                                break;
-                            else{
-                                hash[i][j] = false;
-                                count++
-                            }
-                        }
-                    }
-                }
-                result = result > count? result: count;
+    // The fastest solution to pass all test
+    const theFastest = (array) => {
+        const arr = array;
+        if(array[0] > array[1])
+            arr.sort((a,b) => a-b);
+        let [f, s] = [1, 1];
+        for(let i = 1; i < length; i++){
+            if(arr[i] - 1 === arr[i - 1]){
+                ++f
+            }else if (arr[i] - arr[i - 1] > 1){
+                s = s < f? f: s;
+                f = 1
             }
         }
-    }*/
-    return result
+        return  s = s < f? f: s;
+    };
+
+    //If next algorithm such as you expect,
+    // i want to notice, it more slow for your cases then previous one even when every array is sorted and has complexity more then O(n)
+    const hashMap = (array) =>{
+        let result = 1;
+        for(x of array) {
+            let key1 = Math.floor((x / length));
+            let key2 = x % length;
+            if (!hash[key1])
+                hash[key1] = {};
+            hash[key1][key2] = true;
+            let count = countUp(key1, key2) + countDown(key1, key2);
+            result = result > count ? result : count
+        }
+        return result
+    };
+
+    const countUp = (key1, key2, count = 1) => {
+        key2++;
+        if(hash[key1][key2]){
+            count++;
+            return countUp(key1, key2, count)
+        }
+        if(key2 === length -1 && hash[key1 + 1] && hash[key1][key2])
+            return countDown(key1 + 1, -1, count);
+        return count
+    };
+
+    const countDown = (key1, key2, count = 0) => {
+        key2--;
+        if(hash[key1][key2]){
+            count++;
+            return countDown(key1, key2, count)
+        }
+        if(!key2 && hash[key1 - 1] && hash[key1][key2])
+            return countDown(key1 - 1, length, count);
+        return count
+    };
+
+    return sort? theFastest(array): hashMap(array)
 };
